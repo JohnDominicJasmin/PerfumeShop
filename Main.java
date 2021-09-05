f9eb551e66a0e42633ab95aeba8aa734623aaeb2
John_Dominic <67941562+JohnDominicJasmin@users.noreply.github.com>
2021-09-05 12:12:53 +0000

    Change function names, add askToContinue function

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 class Main {
 
-  private static int userChoice;
-
+  private static int perfumeShopChoice;
+  private static String wishToContinue;
 
 
   public static void main(String[] args) {
@@ -16,12 +16,13 @@ class Main {
     PerfumeShopPrinter.printMainMenu();
 
     try {
-      userChoice = getInput.nextInt();
+      perfumeShopChoice = getInput.nextInt();
     } catch (InputMismatchException e) {
       System.out.println("Input invalid");
     }
 
-    switch (userChoice) {
+  do{
+    switch (perfumeShopChoice) {
       case 1:
       
       try{
@@ -29,23 +30,57 @@ class Main {
         var perfumeStore = new PerfumeStoreProcessor(new ElegantPerfumeStoreWrapper(getInput.nextInt()));
         perfumeStore.purchasePerfume();
         PerfumeShopPrinter.printTotalPrice(perfumeStore.getTotalPrice());
-        PerfumeShopPrinter.printFeeMessage();
+        PerfumeShopPrinter.askFee();
         perfumeStore.setTotalFee(getInput.nextDouble());
         perfumeStore.payTotalPrice();
         perfumeStore.printReceipt();
       }catch(RuntimeException e){
         System.out.println(e.getMessage());
       }
-    //catch for insuffient funds
+   
         break;
 
       case 2:
 
+       try{
+        PerfumeShopPrinter.printElegantPerfumeShopPricesAndPromosMenu();
+        var perfumeStore = new PerfumeStoreProcessor(new OrdinaryPerfumeStoreWrapper(getInput.nextInt()));
+        perfumeStore.purchasePerfume();
+        PerfumeShopPrinter.printTotalPrice(perfumeStore.getTotalPrice());
+        PerfumeShopPrinter.askFee();
+        perfumeStore.setTotalFee(getInput.nextDouble());
+        perfumeStore.payTotalPrice();
+        perfumeStore.printReceipt();
+      }catch(RuntimeException e){
+        System.out.println(e.getMessage());
+      }
+   
         break;
+
+      
       default:
+       System.out.println("Choice doesn't exist!");
     }
+    PerfumeShopPrinter.askToContinue();
+    wishToContinue = getInput.nextLine();
+  }while(wishToContinue == Yes());
+
+  
 
   }
+
+
+
+
+
+  public static String Yes(){
+    String[] arr = {"Y","y","Yes","yes"};
+    for(String answer:arr){
+      return answer;
+    }
+return "";
+  }
+
   // catch error if pay is insufficient
   // handle error if double,long,int
   // print success
@@ -53,4 +88,5 @@ class Main {
   //close scanner
   //reset prices
   //catch if no amount of bottles bought
+  //catch for insuffient funds
 }
--- PerfumeShopPrinter.java
+++ PerfumeShopPrinter.java
@@ -1,6 +1,7 @@
  public class PerfumeShopPrinter{
 
 public static void printMainMenu(){
+  
 System.out.println("\n\n");
 System.out.println("|------------------------------------------------- 👋WELCOME -------------------------------------------------|");
 System.out.println("|                                                                                                             |");
@@ -16,6 +17,8 @@ System.out.print("|  Enter your choice:");
 
 
 public static void printOrdinaryPerfumeShopPricesAndPromosMenu(){
+
+System.out.println("\n|-------------------------------------------------------------------------------------------------------------|");
 System.out.println("|                                              🏪Ordinary Perfume                                             |");
 System.out.println("|                                                                                                             |");
 System.out.println("|  Prices💵:                                                                                                  |");
@@ -26,18 +29,40 @@ System.out.println("|          ₱150.00 = minimum of 5 bottles                 
 System.out.println("|          ₱125.00 = minimum of 10 bottles                                                                    |");
 System.out.println("|      discount %5 = exceeds to 20 bottles                                                                    |");
 System.out.println("|                                                                                                             |");
-System.out.print("|\t👉 Enter amount of perfume bottle:");
+askNumberOfPerfume();
 
 }
 
 
+public static void printElegantPerfumeShopPricesAndPromosMenu(){
 
+  System.out.println("\n|-------------------------------------------------------------------------------------------------------------|");
+ System.out.println("|                                              🏪Elegant Perfume                                             |");
+System.out.println("|                                                                                                             |");
+System.out.println("|  Prices💵:                                                                                                  |");
+System.out.println("|          ₱250.00 = per bottle                                                                                |");
+System.out.println("|                                                                                                             |");
+System.out.println("|   Promo💸:                                                                                                  |");
+System.out.println("|          ₱185.00 = minimum of 5 bottles                                                                     |");
+System.out.println("|          ₱170.00 = minimum of 10 bottles                                                                    |");
+System.out.println("|      discount %5 = exceeds to 20 bottles                                                                    |");
+System.out.println("|                                                                                                             |");
+askNumberOfPerfume();
 
+}
+
+public static void askToContinue(){
+    System.out.print("|\t👉 Would you like to use our System again? [Y/n]");
+}
+
+private static void askNumberOfPerfume(){
+  System.out.print("|\t👉 Enter amount of perfume bottle:");
+}
 public static void printTotalPrice(double totalPrice){
   System.out.printf("Total cost is: ₱%.2f\n",totalPrice);
 }
 
-public static void printFeeMessage(){
+public static void askFee(){
   System.out.print("Enter fee amount:");
 }
 
